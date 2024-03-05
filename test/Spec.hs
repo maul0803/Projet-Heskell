92f85ce8c9fa75bbcaf4bfe48c1dd70856dc27dc
maul0803 <119857424+maul0803@users.noreply.github.com>
2024-03-05 23:03:50 +0100

    Add files via upload

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,12 @@
---Pour tester, il faut lance le Spec.hs dans le src (copier, coller)
 import RLE
 import Statistic.EncodingTree
 import Statistic.Bit
 import qualified LZ.Dictionaries as Dict
-
-buildEncodingTreeExample :: String -> Maybe (EncodingTree Char)
-buildEncodingTreeExample _ = Just $
-    EncodingNode 5
-        (EncodingNode 4
-            (EncodingLeaf 2 'a')
-            (EncodingLeaf 1 'b'))
-        (EncodingLeaf 3 'c')
+import qualified Statistic.Huffman as Statistic.EncodingTree
 
 main :: IO ()
 main = do
-    --Encoding Tree
-    putStrLn $ "Encoding Tree: "
     let text = "abbca"
-        (encodingTreeResult, compressedText) = Statistic.EncodingTree.compress buildEncodingTreeExample text
-        uncompressedText = Statistic.EncodingTree.uncompress (encodingTreeResult, compressedText)
-        longueur_moyenne = meanLength <$> encodingTreeResult
-    putStrLn $ "Texte initial : " ++ show text
-    putStrLn $ "Texte compressé : " ++ show compressedText
-    putStrLn $ "Texte décompressé : " ++ show uncompressedText
-    putStrLn $ "Longueur moyenne : " ++ maybe "Arbre d'encodage non disponible" show longueur_moyenne
     --RLE
     putStrLn $ "RLE : "
     let compressed_text_RLE = RLE.compress text
@@ -31,3 +14,13 @@ main = do
     putStrLn $ "Texte initial : " ++ show text
     putStrLn $ "Texte compressé : " ++ show compressed_text_RLE
     putStrLn $ "Texte décompressé : " ++ show uncompressed_text_RLE
+    --Huffman
+    putStrLn $ "Huffman : "
+    let (encoding_tree_Huffman, compressed_text_Huffman) = Statistic.EncodingTree.compress Statistic.EncodingTree.tree text
+        uncompressed_text_Huffman = Statistic.EncodingTree.uncompress (encoding_tree_Huffman, compressed_text_Huffman)
+        longueur_moyenne_Huffman = meanLength <$> encoding_tree_Huffman
+    putStrLn $ "Arbre d'encodage: " ++ show encoding_tree_Huffman
+    putStrLn $ "Longueur moyenne : " ++ maybe "Arbre d'encodage non disponible" show longueur_moyenne_Huffman
+    putStrLn $ "Texte initial : " ++ show text
+    putStrLn $ "Texte compressé : " ++ show compressed_text_Huffman
+    putStrLn $ "Texte décompressé : " ++ show uncompressed_text_Huffman
