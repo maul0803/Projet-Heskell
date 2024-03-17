d69fe82709673fb3fad5f7641162a87c7ed0ff6c
ciaens <clementchristiaens@pm.me>
2024-03-17 19:53:24 +0100

    Premier test Source.hs

--- src/Statistic/Source.hs
+++ src/Statistic/Source.hs
@@ -6,17 +6,24 @@
 
 module Statistic.Source(occurrences, entropy, orderedCounts) where
 
-import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Map ((!))
+import Data.Maybe (fromMaybe)
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
 -- | The map giving occurrences of each symbol in the source
-occurrences :: Ord a => [a] -> Map a Int
-occurrences _ = undefined -- TODO
+occurrences :: Ord a => [a] -> Map.Map a Int
+occurrences = foldr (\symbol -> Map.insertWith (+) symbol 1) Map.empty
 
 -- | SHANNON entropy of source
 entropy :: Ord a => [a] -> Double
-entropy _ = undefined -- TODO
+entropy source =
+    let occs = occurrences source
+        total = fromIntegral $ sum (Map.elems occs)
+        probs = Map.map (\occ -> let prob = fromIntegral occ / total in prob * logBase 2 prob) occs
+    in negate $ sum (Map.elems probs)
 
 -- | List of occurrences ordered by count
 orderedCounts :: Ord a => [a] -> [(a, Int)]
-orderedCounts _ = undefined -- TODO
-
+orderedCounts = sortBy (comparing snd) . Map.toList . occurrences
