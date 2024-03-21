38bed834ff3972f29efdd83702184d65efdf1cfd
maul0803 <119857424+maul0803@users.noreply.github.com>
2024-03-21 16:17:49 +0100

    Update EncodingTree.hs
    Modification de EncodingTree pour qu'il marche avec des arbres à une seule feuille.

--- src/Statistic/EncodingTree.hs
+++ src/Statistic/EncodingTree.hs
@@ -6,7 +6,7 @@
 module Statistic.EncodingTree(EncodingTree(..), isLeaf, count, has, encode, decodeOnce, decode, meanLength, compress, uncompress) where
 
 import Statistic.Bit
-
+import Data.Maybe (catMaybes)
 data EncodingTree a = EncodingNode Int (EncodingTree a) (EncodingTree a)
                     | EncodingLeaf Int a
   deriving (Eq, Show)
@@ -21,7 +21,6 @@ count :: EncodingTree a -> Int
 count (EncodingLeaf cnt _  ) = cnt
 count (EncodingNode cnt _ _) = cnt
 
---TODO
 -- | Search for symbol in encoding tree
 has :: Eq a => EncodingTree a -> a -> Bool
 has (EncodingLeaf _ x) symbol 
@@ -33,29 +32,63 @@ has (EncodingNode _ left right) symbol = has left symbol || has right symbol
 -- | Computes the binary code of symbol using encoding tree
 -- If computation is not possible, returns `Nothing`.
 encode :: Eq a => EncodingTree a -> a -> Maybe [Bit]
-encode (EncodingLeaf _ x) symbol
+encode tree bits
+  | isMeanLengthZero tree = encodeForLeaf tree bits
+  | otherwise = encodeMain tree bits
+-- Fonction utilisée pour tout les arbres, sauf ceux avec une seule feuille
+encodeMain :: Eq a => EncodingTree a -> a -> Maybe [Bit]
+encodeMain (EncodingLeaf _ x) symbol
   | x == symbol = Just [] -- Le symbole est dans l'arbre, on ajouter rien à la liste
   | otherwise = Nothing -- Le symbole n'est pas danc l'arbre
-encode (EncodingNode _ left right) symbol
-  | has left symbol = fmap (Zero:) (encode left symbol) -- On ajouter Zero à la liste, on cherche à gauche
-  | has right symbol = fmap (One:) (encode right symbol) -- On ajoute One à la list, on cherche à droite
+encodeMain (EncodingNode _ left right) symbol
+  | has left symbol = fmap (Zero:) (encodeMain left symbol) -- On ajouter Zero à la liste, on cherche à gauche
+  | has right symbol = fmap (One:) (encodeMain right symbol) -- On ajoute One à la list, on cherche à droite
   | otherwise = Nothing -- Le symbole n'est pas danc l'arbre
-
+-- Fonction utilisée uniquement pour les arbres avec qu'une seule feuille
+encodeForLeaf :: Eq a => EncodingTree a -> a -> Maybe [Bit]
+encodeForLeaf (EncodingLeaf _ x) symbol
+  | x == symbol = Just [Zero] -- Le symbole est dans l'arbre, on ajouter rien à la liste
+  | otherwise = Nothing -- Le symbole n'est pas danc l'arbre
+encodeForLeaf (EncodingNode _ left right) symbol = Nothing
 
 -- | Computes the first symbol from list of bits using encoding tree and also returns the list of bits still to process
 -- If computation is not possible, returns `Nothing`.
 decodeOnce :: EncodingTree a -> [Bit] -> Maybe (a, [Bit])
-decodeOnce (EncodingLeaf _ symbol) bits = Just (symbol, bits)-- Fin de l'arbre
-decodeOnce (EncodingNode _ left right) (bit:bits)-- On sépare l'array de bits avec [bit,bits]
-  | bit == Zero = decodeOnce left bits
-  | bit == One = decodeOnce right bits
-decodeOnce _ _ = Nothing
+decodeOnce tree bits
+  | isMeanLengthZero tree = decodeOnceForLeaf tree bits
+  | otherwise = decodeOnceMain tree bits
+-- Fonction utilisée pour tout les arbres, sauf ceux avec une seule feuille
+decodeOnceMain :: EncodingTree a -> [Bit] -> Maybe (a, [Bit])
+decodeOnceMain (EncodingLeaf _ symbol) bits = Just (symbol, bits)-- Fin de l'arbre
+decodeOnceMain (EncodingNode _ left right) (bit:bits)-- On sépare l'array de bits avec [bit,bits]
+  | bit == Zero = decodeOnceMain left bits
+  | bit == One = decodeOnceMain right bits
+decodeOnceMain _ _ = Nothing
+-- Fonction utilisée uniquement pour les arbres avec qu'une seule feuille
+decodeOnceForLeaf :: EncodingTree a -> [Bit] -> Maybe (a, [Bit])
+decodeOnceForLeaf (EncodingLeaf _ symbol) (h:t) = Just (symbol, t)-- On a directement une feuille normalement
+decodeOnceForLeaf (EncodingNode _ _ _) _ = Nothing
+
+-- Fonction pour tester si la taille de l'arbre d'encodage est de 0
+isMeanLengthZero :: EncodingTree a -> Bool
+isMeanLengthZero tree = meanLength tree == 0
 
 -- | Computes list of symbols from list of bits using encoding tree
 decode :: EncodingTree a -> [Bit] -> Maybe [a]
-decode _ [] = Just []  -- Cas de base
 decode tree bits
-    | Just (symbol, remainingBits) <- decodeOnce tree bits = fmap (symbol :) (decode tree remainingBits)
+  | isMeanLengthZero tree = decodeForLeaf tree bits
+  | otherwise = decodeMain tree bits
+-- Fonction utilisée pour tout les arbres, sauf ceux avec une seule feuille
+decodeMain :: EncodingTree a -> [Bit] -> Maybe [a]
+decodeMain _ [] = Just []  -- Cas de base
+decodeMain tree bits
+    | Just (symbol, remainingBits) <- decodeOnceMain tree bits = fmap (symbol :) (decodeMain tree remainingBits)
+    | otherwise = Nothing
+-- Fonction utilisée uniquement pour les arbres avec qu'une seule feuille
+decodeForLeaf :: EncodingTree a -> [Bit] -> Maybe [a]
+decodeForLeaf _ [] = Just []  -- Cas de base
+decodeForLeaf tree bits
+    | Just (symbol, remainingBits) <- decodeOnceForLeaf tree bits = fmap (symbol :) (decodeForLeaf tree remainingBits)
     | otherwise = Nothing
 
 -- | Mean length of the binary encoding
