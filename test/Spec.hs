52d0a04304febf9df91d694f6630de3cf246f3c4
maul0803 <119857424+maul0803@users.noreply.github.com>
2024-03-22 22:26:14 +0100

    Update Spec.hs
    Amélioration du test pour lancer tout les strings en une seule fois.
    Clarification du code (GPT), division en 2 fonctions dont main.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,41 +1,45 @@
 import RLE
 import Statistic.EncodingTree
-import qualified LZ.Dictionaries as Dict
 import qualified Statistic.Huffman
 import qualified Statistic.ShannonFano
 
-main :: IO ()
-main = do
-    let text = "abbca" -- Shannon et Huffman pareils
-        --text = "abbcbbb" -- Shannon et Huffman pareils
-        --text = "zzaaabbbbbbcccccccc" -- Shannon et Huffman différents
-        --text = "xbbzzaaabbbcccccccc" -- Shannon et Huffman différents
-        --text = "bzzzaaabbbccccc"  -- Shannon et Huffman pareils
-    --RLE
+-- Fonction pour tester un texte donné
+testText :: String -> IO ()
+testText text = do
+    putStrLn $ "Texte: " ++ text
+    -- RLE
     putStrLn $ "RLE : "
     let compressed_text_RLE = RLE.compress text
         uncompressed_text_RLE = RLE.uncompress compressed_text_RLE
     putStrLn $ "Texte initial : " ++ show text
     putStrLn $ "Texte compressé : " ++ show compressed_text_RLE
     putStrLn $ "Texte décompressé : " ++ show uncompressed_text_RLE
-    --Huffman
+    -- Huffman
     putStrLn $ "Huffman : "
     let (encoding_tree_Huffman, compressed_text_Huffman) = Statistic.EncodingTree.compress Statistic.Huffman.tree text
         uncompressed_text_Huffman = Statistic.EncodingTree.uncompress (encoding_tree_Huffman, compressed_text_Huffman)
         longueur_moyenne_Huffman = meanLength <$> encoding_tree_Huffman
-    --putStrLn $ "Arbre d'encodage: " ++ show encoding_tree_Huffman
     putStrLn $ "Arbre d'encodage: " ++ show encoding_tree_Huffman
     putStrLn $ "Longueur moyenne : " ++ maybe "Arbre d'encodage non disponible" show longueur_moyenne_Huffman
-    putStrLn $ "Texte initial : " ++ show text
     putStrLn $ "Texte compressé : " ++ show compressed_text_Huffman
     putStrLn $ "Texte décompressé : " ++ show uncompressed_text_Huffman
-    --Shannon
+    -- Shannon
     putStrLn $ "Shannon : "
     let (encoding_tree_Shannon, compressed_text_Shannon) = Statistic.EncodingTree.compress Statistic.ShannonFano.tree text
         uncompressed_text_Shannon = Statistic.EncodingTree.uncompress (encoding_tree_Shannon, compressed_text_Shannon)
         longueur_moyenne_Shannon = meanLength <$> encoding_tree_Shannon
     putStrLn $ "Arbre d'encodage: " ++ show encoding_tree_Shannon
     putStrLn $ "Longueur moyenne : " ++ maybe "Arbre d'encodage non disponible" show longueur_moyenne_Shannon
-    putStrLn $ "Texte initial : " ++ show text
     putStrLn $ "Texte compressé : " ++ show compressed_text_Shannon
     putStrLn $ "Texte décompressé : " ++ show uncompressed_text_Shannon
+    putStrLn $ "---------------------------------------------------------------------------------------------------"
+
+main :: IO ()
+main = do
+    -- Testez tous les textes en commentaire
+    testText "abbca" -- Shannon et Huffman pareils
+    testText "abbcbbb" -- Shannon et Huffman pareils
+    testText "zzaaabbbbbbcccccccc" -- Shannon et Huffman différents
+    testText "xbbzzaaabbbcccccccc" -- Shannon et Huffman différents
+    testText "bzzzaaabbbccccc"  -- Shannon et Huffman pareils
+    testText "aaaaa"
