87e200d13df964d81c5af653b15a0484c34b7888
Lucaslas28 <71724891+Lucaslas28@users.noreply.github.com>
2024-03-24 01:08:55 +0100

    Aussi oublié nothing

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -5,11 +5,59 @@
 -}
 module LZ.LZW(compress, uncompress) where
 
+import LZ.Dictionaries (empty, ascii, zeroAsChar)
+import Data.List (elemIndex)
+
+type Dictionary = [String]
+
 -- | LZW compress method
+
 compress :: String -> [Int]
-compress _ = undefined -- TODO
+compress [] = []
+compress text = compress' "" ascii text []
+
+compress' :: String -> Dictionary -> String -> [Int] -> [Int]
+compress' acc dict [] output
+  | acc == [] = output
+	| stringInDictionary dict acc = output++[indexOf acc dict]
+	| otherwise = output++[indexOf (init acc) dict]++[indexOf [(last acc)] dict]
+compress' "" dict (x:xs) output = compress' [x] dict xs output                -- CASE 1 : First reading of the input
+compress' acc dict (x:xs) output                                              -- CASE 2 : Reading of the input
+	| stringInDictionary dict acc = aux acc dict (x:xs) output	                -- SUB-CASE 2.a : The current string (acc) is in the Dictionary
+	| otherwise = compress' ([(last acc)]++[x]) (dict++[acc]) xs (output++[indexOf (init acc) dict])  	-- SUB-CASE 2.b : The current string (acc) is not in the Dictionary
+
+
+--Aux :: String -> Dictionary -> String -> [Int]
+--Pour prévenir si length(acc) > 3 et acc++[x] € dict par exemple dans "belle echellelle"
+aux acc dict (x:xs) output
+  | stringInDictionary dict (acc++[x]) = compress' (acc++[x]) dict xs output
+  | otherwise = compress' (acc++[x]) dict xs output
+
+-- Get the index of an element in a Dictionary
+indexOf :: String -> Dictionary -> Int
+indexOf element dict =
+  case elemIndex element dict of
+    Just index -> index
+    Nothing     -> -1
+
+-- Verify if a String is in a Dictionary
+stringInDictionary :: Dictionary -> String -> Bool
+stringInDictionary dict str = str `elem` dict
+
+
+valueAtIndex :: Int -> Dictionary -> String
+valueAtIndex i dict = dict !! i
+
 
 -- | LZW uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [Int] -> Maybe String
-uncompress _ = undefined -- TODO
+uncompress array = uncompress' [] array ascii []
+
+uncompress' :: String -> [Int] -> Dictionary -> String -> Maybe String
+uncompress' w [] dict output = Just output
+uncompress' w (x:xs) dict [] = uncompress' (valueAtIndex x dict) xs dict (valueAtIndex x dict )
+
+uncompress' w (x:xs) dict output
+  | x < length dict = uncompress' (valueAtIndex x dict) xs (dict++[w++[head (valueAtIndex x dict)]]) (output++(valueAtIndex x dict))
+  | otherwise = uncompress' (w++[head w]) xs (dict++[w++[head (w++[head w])]]) (output++(w++[head w]))
